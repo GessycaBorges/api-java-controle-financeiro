63280ec3c9449856648a93c93cd45c3103361238
Gessyca Borges <gessyca.borges@gmail.com>
2024-07-28 21:48:24 -0300

    Criação e importação do Projeto

---
+++ src/test/java/com/api_java_controle_financeiro/ApiJavaControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_java_controle_financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
